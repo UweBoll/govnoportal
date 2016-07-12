87b2feda5a1928b2c1a253c030fe47c4a6e14e09
UweBoll <semsev@bk.ru>
2016-07-12 22:12:31 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>govno-portal.vl</title>
+</head>
+<body>
+	<div class="wrapper">
+		<h1>hello, world!</h1>
+	</div>
+</body>
+</html>
